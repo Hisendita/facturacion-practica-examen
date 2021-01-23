e25a6b118cada343c299c0cc711d0b68f96b3fdb
sergi <lancerosergi@gmail.com>
2021-01-23 16:04:48 +0100

    added modelo Productos

---
+++ models/Productos.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+
+class Productos(models.Model):
+    _name = 'examen3.productos'
+    _description = 'Productos'
+    
+    name = fields.Char(string="Nombre del producto")
+    desc = fields.Html(string="Descripcion del producto")
+    pvp = fields.Float(string="Precio de venta", default="0,00")
+    
+    
